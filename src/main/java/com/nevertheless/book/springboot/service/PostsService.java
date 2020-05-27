52dfcb43a93590174ed617939d06350251440b9a
sspark <sspark@tpirates.com>
2020-05-27 15:10:09 +0900

    PostsApiController - Posts 등록 TEST

---
+++ src/main/java/com/nevertheless/book/springboot/service/PostsService.java
@@ -0,0 +1,19 @@
+package com.nevertheless.book.springboot.service;
+
+import com.nevertheless.book.springboot.domain.posts.PostsRepository;
+import com.nevertheless.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
